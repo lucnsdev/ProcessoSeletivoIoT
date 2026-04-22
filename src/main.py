932f3ffd2d754d0dc0a0ddfd61bcb3926aa73c56
Lucas Nascimento <lucns.dev@gmail.com>
2026-04-22 13:58:37 -0300

    Update print statement in main.py
    Changed print statement to display 'Teste' instead of 'Simulacao finalizada'.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 # @Developed by @lucns
 
-print("Simulacao finalizada.") # Sem isso o Github Actions rejeita a importação de arquivos secundarios.
+print("Teste") # Sem isso o Github Actions rejeita a importação de arquivos secundarios.
 
 import time
 from tm1637 import TM1637
